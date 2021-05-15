43872becdae89307e410dde6db9aea0e2497d10c
Nguyễn Tịnh <43327850+1660623@users.noreply.github.com>
2021-05-15 15:33:11 +0700

    add new idea

--- lib/my.rb
+++ lib/my.rb
@@ -4,6 +4,45 @@ require_relative "my/version"
 
 module My
   class Error < StandardError; end
+  class Song
+    
+    def initialize(name, author, vol)
+      @name = name
+      @author = author
+      @vol = 1
+    end
+  end
   # Your code goes here...
-  p "ans"
+  class SongList
+    def initialize
+      @songs = Array.new
+    end
+  
+    def append(aSong)
+      @songs.push(aSong)
+    end
+
+    def deleteFirst
+      @songs.shift
+    end
+
+    def deleteLast
+      @songs.pop
+    end
+
+    def [](key)
+        if key.kind_of?(Integer)
+          result = @songs[key]
+        else
+          @song.find { |aSong| key == aSong.author}
+        end
+        return result
+    end
+  end
+
+  list = SongList.new
+  list.append(Song.new('title1', 'atist1', 1))
+  puts list.inspect
+  # see all methods of class
+  p list.methods
 end

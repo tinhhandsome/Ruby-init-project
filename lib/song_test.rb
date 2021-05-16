471be77e3e04cdba9a9ecf7eff98c4cae7add20d
Nguyễn Tịnh <43327850+1660623@users.noreply.github.com>
2021-05-16 12:18:05 +0700

    add new Bolero Song

--- lib/song_test.rb
+++ lib/song_test.rb
@@ -22,9 +22,22 @@ module SongTest
       "KS: #{@name}--#{@artist} (#{@duration}) [#{@lyrics}]"
     end
   end
+
+  class BoleroSong < Song
+    def initialize(name, artist, duration, lyrics)
+      super(name, artist, duration)
+      @lyrics = lyrics
+    end
+
+    def to_s
+      super + " #{@lyrics}"
+    end
+  end
   # phương thức to_s có thể ghi đè constructor của class kế thừa
   # KaraokeSong kế thừa Song
   # aSong = Song.new("Bicylops", "Fleck", 260)
   aSong = KaraokeSong.new("My Way", "Sinatra", 255, "And now, the...")
+  aBoleroSong = BoleroSong.new("Tan Co Dao Duyen", "Thuan Minh", 244, "And now, the...")
   p aSong.to_s
+  p aBoleroSong.to_s
 end

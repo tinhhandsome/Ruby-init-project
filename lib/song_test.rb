cef730c5186afdfd03e53673bdf22c227cb63a1d
Nguyễn Tịnh <43327850+1660623@users.noreply.github.com>
2021-05-16 12:11:14 +0700

    add new idea

--- lib/song_test.rb
+++ lib/song_test.rb
@@ -12,6 +12,19 @@ module SongTest
     end
   end
 
-  aSong = Song.new("Bicylops", "Fleck", 260)
+  class KaraokeSong < Song
+    def initialize(name, artist, duration, lyrics)
+      super(name, artist, duration)
+      @lyrics = lyrics
+    end
+
+    def to_s
+      "KS: #{@name}--#{@artist} (#{@duration}) [#{@lyrics}]"
+    end
+  end
+  # phương thức to_s có thể ghi đè constructor của class kế thừa
+  # KaraokeSong kế thừa Song
+  # aSong = Song.new("Bicylops", "Fleck", 260)
+  aSong = KaraokeSong.new("My Way", "Sinatra", 255, "And now, the...")
   p aSong.to_s
 end
